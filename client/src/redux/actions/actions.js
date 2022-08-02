35de5ea3e3fbab1e701fc51aae583d23189c8dfd
Damebeus <99093997+Damebeus@users.noreply.github.com>
2022-08-02 19:58:11 -0300

    enrique

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export function getCountries() {
   return async function (dispatch) {
-    const response = await axios.get("http://localhost:3001/countries");
+    const response = await axios.get(
+      "https://ebeus-countries.herokuapp.com/countries"
+    );
     return dispatch({
       type: "GET_COUNTRIES",
       payload: response.data,
@@ -12,7 +14,9 @@ export function getCountries() {
 
 export function getActivities() {
   return async function (dispatch) {
-    const response = await axios.get("http://localhost:3001/activity");
+    const response = await axios.get(
+      "https://ebeus-countries.herokuapp.com/activity"
+    );
     return dispatch({
       type: "GET_ACTIVITIES",
       payload: response.data,
@@ -24,7 +28,7 @@ export function getNameCountries(name) {
   return async function (dispatch) {
     try {
       var response = await axios.get(
-        `http://localhost:3001/countries?name=${name}`
+        `https://ebeus-countries.herokuapp.com/countries?name=${name}`
       );
       return dispatch({
         type: "GET_NAME_COUNTRIES",
@@ -37,20 +41,25 @@ export function getNameCountries(name) {
 }
 export function postActivities(payload) {
   return async function (dispatch) {
-    const response = await axios.post("http://localhost:3001/activity", {
-      name: payload.name,
-      duration: payload.duration,
-      difficulty: payload.difficulty,
-      season: payload.season,
-      countries: payload.id,
-    });
+    const response = await axios.post(
+      "https://ebeus-countries.herokuapp.com/activity",
+      {
+        name: payload.name,
+        duration: payload.duration,
+        difficulty: payload.difficulty,
+        season: payload.season,
+        countries: payload.id,
+      }
+    );
     return response;
   };
 }
 export function getDetail(id) {
   return async function (dispatch) {
     try {
-      var response = await axios.get(`http://localhost:3001/countries/${id}`);
+      var response = await axios.get(
+        `https://ebeus-countries.herokuapp.com/countries/${id}`
+      );
       return dispatch({
         type: "GET_DETAIL",
         payload: response.data,
